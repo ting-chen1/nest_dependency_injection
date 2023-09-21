b0f4a4fba49f03d296d539e36a570c1d8a8b984f
ting-chen1 <ting.chen@nextlink.com.tw>
2023-09-21 14:01:06 +0800

    ComputerModule import CupModule and DiskModule

--- src/cpu/cpu.module.ts
+++ src/cpu/cpu.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CpuService } from './cpu.service';
 import { PowerModule } from '../power/power.module';
+import { CpuService } from './cpu.service';
 
 @Module({
   imports: [PowerModule],
   providers: [CpuService],
+  exports: [CpuService],
 })
 export class CpuModule {}
